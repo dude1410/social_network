f49ff204713cfe7ce54510eb23a28bf5d8155bb4
ved.ma <sleep.dora@gmail.com>
2021-06-01 12:06:03 +0300

    Git Init

--- src/main/java/JavaPRO/model/Notification.java
+++ src/main/java/JavaPRO/model/Notification.java
@@ -19,6 +19,7 @@ public class Notification implements Serializable {
     @Column(name = "id", nullable = false)
     private int id;
 
+
     @Enumerated(EnumType.STRING)
     @JoinColumn(name = "notification_type", nullable = false)
     private NotificationType notificationType;
