022eaa0aa5a14afb1dfd8221f80f9f8d86daa928
dude <dude1410@mail.ru>
2021-06-03 20:39:05 +0300

    mail_support #5

--- src/main/java/JavaPRO/services/EmailService.java
+++ src/main/java/JavaPRO/services/EmailService.java
@@ -11,13 +11,18 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 @Service
 public class EmailService {
 
     @Autowired
     JavaMailSender javaMailSender;
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     @Value("${spring.mail.username}")
     private String username;
@@ -26,7 +31,7 @@ public class EmailService {
         new Thread(() -> {
             MimeMessage message = javaMailSender.createMimeMessage();
             try {
-                MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+                MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8"); // todo: change
                 messageHelper.setFrom(username);
                 messageHelper.setTo(email);
                 messageHelper.setSubject(subject);
@@ -44,11 +49,22 @@ public class EmailService {
                     .badRequest()
                     .body(new MailSupportResponse(false, Config.STRING_MAIL_TO_SUPPORT_NO_EMAIL));
         }
-        if (mailSupportRequest.getText() == null || mailSupportRequest.getText().isEmpty()) {
+        if (!VALID_EMAIL_ADDRESS_REGEX.matcher(mailSupportRequest.getEmail()).matches()){
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MailSupportResponse(false, Config.STRING_MAIL_TO_SUPPORT_NO_EMAIL));
+        }
+        if (mailSupportRequest.getText() == null) {
             return ResponseEntity
                     .badRequest()
                     .body(new MailSupportResponse(false, Config.STRING_MAIL_TO_SUPPORT_NO_TEXT));
         }
+        if (mailSupportRequest.getText().length() < 20) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MailSupportResponse(false, Config.STRING_MAIL_TO_SUPPORT_NO_TEXT));
+        }
+        System.out.println(mailSupportRequest.getText());
        sendMail((Config.STRING_MAIL_TO_SUPPORT_SUBJECT + mailSupportRequest.getEmail()),
                mailSupportRequest.getText(),
                mailSupportRequest.getEmail()); // todo: "request.getEmail" change to "username"
