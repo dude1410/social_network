5599c254120d7b1675f433095e519cd914a5fc67
kshinobi <koshaevk@gmail.com>
2021-06-08 12:57:29 +0000

    Revert "Merge branch 'workbranch' into 'master'"
    This reverts merge request !32

--- src/main/java/JavaPRO/controller/AuthController.java
+++ src/main/java/JavaPRO/controller/AuthController.java
@@ -1,13 +1,9 @@
 package JavaPRO.controller;
 
-import JavaPRO.api.response.ErrorResponse;
 import JavaPRO.api.response.Response;
-import JavaPRO.config.Config;
 import JavaPRO.model.DTO.Auth.UnauthorizedPersonDTO;
 import JavaPRO.services.AuthService;
 import io.swagger.annotations.Api;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -15,12 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 @RestController
 @Api(value = "/api/v1/auth")
 public class AuthController {
-    private final Logger logger = LogManager.getLogger(AuthService.class);
+
     private final AuthService authService;
 
 
@@ -31,17 +25,9 @@ public class AuthController {
     @PostMapping(value = "/api/v1/auth/login",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Response> login(@Valid @RequestBody UnauthorizedPersonDTO user, Errors error) {
-
-        if (error.hasErrors()) {
-            logger.error(String.format("Error in request  '%s'", error.getFieldError()));
-            return ResponseEntity
-                    .badRequest()
-                    .body(new ErrorResponse("invalid_request", Config.STRING_USER_ISBLOCKED));
-        }
-        return authService.loginUser(user);
+    public ResponseEntity<Response> login(@RequestBody UnauthorizedPersonDTO user, Errors error) {
+        return authService.loginUser(user, error);
     }
-
 
     @PostMapping(value = "/api/v1/auth/logout",
             produces = MediaType.APPLICATION_JSON_VALUE)
---
+++ src/test/java/JavaPRO/StudyGroup12ApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaPRO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGroup12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
