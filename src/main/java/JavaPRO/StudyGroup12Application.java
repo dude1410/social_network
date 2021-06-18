cef37734bd6b426ddd2f4675b1c1ede14720f687
ved.ma <sleep.dora@gmail.com>
2021-06-18 19:41:33 +0300

    Сортируем посты

--- src/main/java/JavaPRO/StudyGroup12Application.java
+++ src/main/java/JavaPRO/StudyGroup12Application.java
@@ -3,10 +3,18 @@ package JavaPRO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class StudyGroup12Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyGroup12Application.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
 	}
 }
--- src/main/java/JavaPRO/api/response/PostShortResponse.java
+++ src/main/java/JavaPRO/api/response/PostShortResponse.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class PostUpdateResponse extends Response{
+public class PostShortResponse extends Response{
 
     private String error;
 
