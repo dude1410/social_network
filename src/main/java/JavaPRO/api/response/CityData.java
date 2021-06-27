4c6d98f1709001b07f8f4cf45957b12bd13b12a7
MaksimKarachun <maksim.karachun@bk.ru>
2021-06-27 16:05:35 +0300

    add findUserById hanfdler

---
+++ src/main/java/JavaPRO/api/response/CityData.java
@@ -0,0 +1,14 @@
+package JavaPRO.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CityData {
+
+    Integer id;
+    String title;
+}
