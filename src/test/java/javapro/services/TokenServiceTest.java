2f2359210bcf13efd7a50aebb057eaf83f2ea2f0
MaksimKarachun <maksim.karachun@bk.ru>
2021-07-19 23:51:42 +0300

    fix token service test

--- src/test/java/javapro/services/TokenServiceTest.java
+++ src/test/java/javapro/services/TokenServiceTest.java
@@ -4,94 +4,91 @@ import javapro.configs.MainTestConfig;
 import javapro.model.Person;
 import javapro.model.Token;
 import javapro.repository.TokenRepository;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.RepeatedTest;
-import org.slf4j.Logger;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import java.util.Date;
-import java.util.HashSet;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.matchesPattern;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = {TokenRepository.class, TokenService.class})
 @Import(MainTestConfig.class)
 class TokenServiceTest {
 
-    private final int TOTAL_ITERATIONS = 500;
-    private final int MAX_TOKEN_LENGTH = 50;
-    private static HashSet<String> tokenSet;
     private final TokenService testableService;
 
-    @Autowired
-    Logger logger;
+    @MockBean
+    private TokenRepository tokenRepository;
 
     @MockBean
-    TokenRepository tokenRepository;
-
-    @MockBean
-    PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     public TokenServiceTest(TokenService testableService) {
         this.testableService = testableService;
     }
 
-    @BeforeAll
-    static void setUpGetTokenTest() {
-        tokenSet = new HashSet<>();
+    @Test
+    void testCheckTokenValidToken() {
+        String token = "validToken";
+        Token tokenReturnedByRepo = new Token();
+        tokenReturnedByRepo.setDate(new Date());
+        when(tokenRepository.findByToken(token)).thenReturn(tokenReturnedByRepo);
+        assertTrue(testableService.checkToken(token));
     }
 
-    @RepeatedTest(TOTAL_ITERATIONS)
-    void testGetRandomString() {
-        try {
-            String token = testableService.getToken();
-            logger.info("Generated new token: " + token);
-            assertNotNull(token);
-            logger.info("token not null - OK");
-            assertTrue(token.length() > 0 && token.length() <= MAX_TOKEN_LENGTH);
-            logger.info("token length valid - OK");
-            assertThat(token, matchesPattern("[a-zA-Z0-9]+"));
-            logger.info("token matches pattern - OK");
-            assertTrue(tokenSet.add(token));
-            logger.info("generated token is unique - OK");
-        } catch (InterruptedException e) {
-            //sad but true...
-            e.printStackTrace();
-        }
+    @Test
+    void testCheckTokenNotValidToken() {
+        String token = "notValidToken";
+        Token tokenReturnedByRepo = new Token();
+        tokenReturnedByRepo.setDate(new Date(System.currentTimeMillis() - 800000));
+        when(tokenRepository.findByToken(token)).thenReturn(tokenReturnedByRepo);
+        assertFalse(testableService.checkToken(token));
     }
 
-    @RepeatedTest(TOTAL_ITERATIONS)
-    void testCheckToken() {
-        try {
-            String token = testableService.getToken();
-            logger.info("got test token: " + token);
-            Token tokenReturnedByRepo = new Token();
-            tokenReturnedByRepo.setToken(token);
-            tokenReturnedByRepo.setDate(new Date());
-            Person testPerson = new Person();
-            testPerson.setRole(0);
-            tokenReturnedByRepo.setPerson(testPerson);
-            logger.info("prepared Token obj: " + tokenReturnedByRepo);
-            when(tokenRepository.findByToken(token)).thenReturn(tokenReturnedByRepo);
-            assertTrue(testableService.checkToken(token));
-            logger.info("check token " + tokenReturnedByRepo + " - OK");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    @Test
+    void testCheckTokenNullToken() {
+        String token = "emptyToken";
+        when(tokenRepository.findByToken(token)).thenReturn(null);
+        assertFalse(testableService.checkToken(token));
     }
 
-    @AfterAll
-    static void invalidateTestTokenSet() {
-        tokenSet = null;
+    @Test
+    void testSetNewPersonToken() {
+        Person person = new Person();
+        Token oldToken = new Token();
+        Token newToken = new Token();
+        oldToken.setToken("oldToken");
+        newToken.setToken("newToken");
+        person.setToken(oldToken);
+        person.setEmail("personEmail");
+        verify(passwordEncoder, atMostOnce()).encode(person.getEmail());
+        when(tokenRepository.findByPerson(person)).thenReturn(newToken);
+        assertNotEquals(oldToken,
+                        testableService.setNewPersonToken(person));
+    }
+
+    @Test
+    void testSetNewPersonTokenNullToken() {
+        Person person = new Person();
+        Token newToken = new Token();
+        newToken.setToken("newToken");
+        person.setToken(null);
+        person.setEmail("personEmail");
+        verify(passwordEncoder, atMostOnce()).encode(person.getEmail());
+        when(tokenRepository.findByPerson(person)).thenReturn(newToken);
+        assertNotNull(testableService.setNewPersonToken(person).getToken());
+    }
+
+    @Test
+    void testFindByToken() {
+        Token token = new Token();
+        token.setToken("testToken");
+        when(tokenRepository.findByToken("testToken")).thenReturn(token);
+        assertEquals(token.getToken(),
+                     testableService.findToken("testToken").getToken());
     }
 }
