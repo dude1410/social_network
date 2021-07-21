bcf3e463a6446f47fbec78b8ef95893d216ac362
MaksimKarachun <maksim.karachun@bk.ru>
2021-07-21 22:42:21 +0300

    remove token from person

--- src/main/java/javapro/model/Person.java
+++ src/main/java/javapro/model/Person.java
@@ -76,9 +76,6 @@ public class Person implements Serializable {
     @Column(name = "role", nullable = false)
     private Integer role;
 
-    @OneToOne(mappedBy = "person")
-    private Token token;
-
     public Role getRole() {
         if (role == 0) {
             return Role.USER;
