45921ad998d9fd973e13e3faa23bdfb134c9c328
Aleksey <89217887567@yandex.ru>
2021-07-25 13:10:59 +0300

    rewritten autotests for registration

---
+++ target/classes/db.changelog/changes/db.changelog_add_friendship.sql
@@ -0,0 +1,15 @@
+insert into friendship (status, src_person_id, dst_person_id) values ('FRIEND', '108', '109');
+
+insert into friendship (status, src_person_id, dst_person_id) values ('FRIEND', '110', '108');
+
+insert into friendship (status, src_person_id, dst_person_id) values ('FRIEND', '111', '108');
+
+insert into friendship (status, src_person_id, dst_person_id) values ('FRIEND', '112', '108');
+
+insert into friendship (status, src_person_id, dst_person_id) values ('FRIEND', '113', '108');
+
+insert into friendship (status, src_person_id, dst_person_id) values ('REQUEST', '114', '108');
+
+insert into friendship (status, src_person_id, dst_person_id) values ('DECLINED', '115', '108');
+
+insert into friendship (status, src_person_id, dst_person_id) values ('SUBSCRIBED', '116', '108');
