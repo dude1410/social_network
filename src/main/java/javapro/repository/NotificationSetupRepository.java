e81de85b9dca895f5324fe4fcbc4dffc4c50198f
olegprokhorov <igarik10@yandex.ru>
2021-08-17 20:14:19 +0700

    fix del_person++

--- src/main/java/javapro/repository/NotificationSetupRepository.java
+++ src/main/java/javapro/repository/NotificationSetupRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface NotificationSetupRepository extends JpaRepository<NotificationSetup, Long> {
 
 
@@ -18,6 +20,7 @@ public interface NotificationSetupRepository extends JpaRepository<NotificationS
     NotificationSetup findByNotificationtypeAndPersonId(String notificationType, Integer personId);
 
     @Modifying
+    @Transactional
     @Query("delete " +
             "from NotificationSetup ns " +
             "where ns.personId = :personId")
